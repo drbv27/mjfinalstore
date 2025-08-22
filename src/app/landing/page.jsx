07fe26ca26416daa666ed47fbc88c2dc73a4c94b
Diego Ricardo Bonilla Villa <drbv27@gmail.com>
2025-08-21 20:27:30 -0500

    repaso de zustand con un contador

--- src/app/landing/page.jsx
+++ src/app/landing/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Counter from "@/components/Counter";
+
 const LandingPage = () => {
   return (
     <div className="ml-auto mr-auto mt-24">
@@ -7,6 +9,7 @@ const LandingPage = () => {
       <p className="font-semibold text-teal-800">
         Encuentra los mejores productos para todas tus necesidades
       </p>
+      <Counter />
     </div>
   );
 };
--- src/app/page copy.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <main>
-        <h1>Hola soy page</h1>
-      </main>
-    </div>
-  );
-}
