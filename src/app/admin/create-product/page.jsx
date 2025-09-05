665cb3d4900c50d4599f17057425348c0a34c241
Diego Ricardo Bonilla Villa <drbv27@gmail.com>
2025-09-04 20:02:33 -0500

    Fix create products

--- src/app/admin/create-product/page.jsx
+++ src/app/admin/create-product/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import axios from "axios";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
@@ -15,7 +15,7 @@ const CreateProductPage = () => {
   });
   const [imageFile, setImageFile] = useState(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
-  /* const router = useRouter(); */
+  const router = useRouter();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -40,7 +40,21 @@ const CreateProductPage = () => {
     formData.append("category", productData.category);
     formData.append("image", imageFile);
 
-    console.log(formData);
+    try {
+      //await axios.post("1 ruta interna de mi back",2 aqui la informacion,3 aqui van los headers)
+      await axios.post("/api/products", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      alert("Producto creado con exito");
+      router.push("/products");
+    } catch (error) {
+      console.error("Error al crear el producto", error);
+      alert("Hubo un problema al guardar el producto", error);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
@@ -50,30 +64,35 @@ const CreateProductPage = () => {
         <Input
           type="text"
           placeholder="Escribe el nombre del producto"
+          name="title"
           value={productData.title}
           onChange={handleChange}
         />
         <Textarea
           placeholder="Escribe la descripcion del producto."
           rows={7}
+          name="description"
           value={productData.description}
           onChange={handleChange}
         />
         <Input
           type="number"
           placeholder="Escribe el precio del producto"
+          name="price"
           value={productData.price}
           onChange={handleChange}
         />
         <Input
           type="text"
           placeholder="Escribe la categoria del producto"
+          name="category"
           value={productData.category}
           onChange={handleChange}
         />
         <Input
           type="file"
           placeholder="Elige la foto del producto"
+          name="image"
           onChange={handleFileChange}
         />
         <Button>{isSubmitting ? "Guardando" : "Guardar Producto"}</Button>
